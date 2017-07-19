b0fff42eeb0c74c34ef01a96dff5ee3c1322174b
annalaise <annalaise.gibbons@gmail.com>
2017-07-19 14:12:29 +0100

    adding test helper

--- spec/features/view_hp_spec.rb
+++ spec/features/view_hp_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario "expects /play to display hit points" do
-    visit('/')
-    fill_in :player_1_name, with: "Mike"
-    fill_in :player_2_name, with: "Tom"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_text("10HP")
   end
 end
