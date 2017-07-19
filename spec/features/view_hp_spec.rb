9c5ce4c0207b0c11a8a074c919feff44caa0168f
Ryan Davidson <daviryan@aol.com>
2017-07-19 16:24:02 +0100

    default hp from player class

--- spec/features/view_hp_spec.rb
+++ spec/features/view_hp_spec.rb
@@ -1,6 +1,7 @@
+require_relative '../../lib/player.rb'
 feature 'View hit points' do
   scenario "expects /play to display hit points" do
     sign_in_and_play
-    expect(page).to have_text("10HP")
+    expect(page).to have_text(Player::DEFAULT_HP.to_s + "HP")
   end
 end
